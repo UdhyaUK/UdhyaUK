d7dcf9f7314957d73035156e0f007dbf5a0967ab
UdhyaUK <udhya59@gmail.com>
2022-01-01 10:26:16 +0530

    Password Functionality

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -2,4 +2,14 @@ package org.git;
 
 public class Git {
 
+	public static void password() {
+		System.out.println("pass");
+	}
+
+	public static void main(String[] args) {
+		
+		password();
+		
+	}
+	
 }
