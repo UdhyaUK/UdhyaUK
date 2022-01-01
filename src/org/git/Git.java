2bddebaca1040412bc32b7b5128f01e128b15a49
UdhyaUK <udhya59@gmail.com>
2022-01-01 10:16:24 +0530

    Username Functionality

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -2,4 +2,14 @@ package org.git;
 
 public class Git {
 
+	public static void username() {
+		System.out.println("username");
+	}
+
+	public static void main(String[] args) {
+
+		username();
+
+	}
+
 }
